7fe4257635fafca2cefb66d79478bc1eab742ce5
rinha7 <eocjstnals12@gmail.com>
2020-09-07 22:07:26 +0900

    completed version
    pytho aiohttp polls demo complete versioin

--- polls/aiohttpdemo_polls/settings.py
+++ polls/aiohttpdemo_polls/settings.py
@@ -1,12 +1,25 @@
-# aiohttpdemo_polls/settings.py
+# settings.py
+import argparse
 import pathlib
-import yaml
 
-config_path = "../config/polls.yaml"
+from trafaret_config import commandline
 
-def get_config(path):
-    with open(path) as f:
-        config = yaml.safe_load(f)
+from aiohttpdemo_polls.utils import TRAFARET
+
+
+BASE_DIR = pathlib.Path(__file__).parent.parent
+DEFAULT_CONFIG_PATH = BASE_DIR / 'config' / 'polls.yaml'
+
+
+def get_config(argv=None):
+    ap = argparse.ArgumentParser()
+    commandline.standard_argparse_options(
+        ap,
+        default_config=DEFAULT_CONFIG_PATH
+    )
+
+    # ignore unknown options
+    options, unknown = ap.parse_known_args(argv)
+
+    config = commandline.config_from_options(options, TRAFARET)
     return config
-
-config = get_config(config_path)
